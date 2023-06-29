f12c9941c045730d64b5e01da9455ef127921f21
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2023-06-29 23:48:58 +0530

    updated
    updated

--- controller/rideSearch.js
+++ controller/rideSearch.js
@@ -5,17 +5,28 @@ import FCM from "fcm-node";
 
 const searchDrivers = async (req, res) => {
     try {
-        const { currentCity, destination, pickupTime, seats } = req.body;
+        const { currentCity, destination, pickupTime, vehicleType, seats } = req.body;
         const date = new Date(pickupTime);
         date.setMinutes(date.getMinutes() - date.getTimezoneOffset()); // Adjust timezone offset to 0
         const pickup = date.toISOString();
-        const driverList = await prisma.$queryRaw(
-            Prisma.sql`SELECT * FROM "Driver" d
-                  WHERE d."currentCity" = ${currentCity}
-                    AND d.destination = ${destination}
-                    AND d."pickupTime" BETWEEN (${pickup}::timestamp - INTERVAL '30 minutes') AND (${pickup}::timestamp + INTERVAL '30 minutes')
-                    AND d.seats > ${seats - 1}`
-        );
+        let driverList = []
+        if(vehicleType == "ANY"){
+             driverList = await prisma.$queryRaw(
+                Prisma.sql`SELECT * FROM "Driver" d
+                      WHERE d."currentCity" = ${currentCity}
+                        AND d.destination = ${destination}
+                        AND d."pickupTime" BETWEEN (${pickup}::timestamp - INTERVAL '30 minutes') AND (${pickup}::timestamp + INTERVAL '30 minutes')
+                        AND d.seats > ${seats - 1}`
+            );
+        }else{
+            driverList = await prisma.$queryRaw(
+                Prisma.sql`SELECT * FROM "Driver" d
+                      WHERE d."currentCity" = ${currentCity}
+                        AND d.destination = ${destination}
+                        AND d."pickupTime" BETWEEN (${pickup}::timestamp - INTERVAL '30 minutes') AND (${pickup}::timestamp + INTERVAL '30 minutes')
+                        AND d.seats > ${seats - 1} and d."vehicleType" = '${Prisma.raw(vehicleType)}'`
+            );
+        }
         res.status(200).json({ message: 'Successfully sent with response', data: driverList });
     } catch (err) {
         console.error(err);
